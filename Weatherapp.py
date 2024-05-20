16b59191b34437026b59d0c869348f90fa9f1cc0
Ayesh Irunda <cohndse232f-100@student.nibm.lk>
2024-05-20 23:52:42 +0530

    basic Gui

--- Weatherapp.py
+++ Weatherapp.py
@@ -26,5 +26,40 @@ top = (display_height - 500) / 2
 
 weatherapp.geometry(f'900x500+{int(left)}+{int(top)}')
 weatherapp.resizable(False, False)
+weatherapp.configure(bg='blue')
+
+style = ttk.Style()
+
+search_bar = ttk.Entry(weatherapp, width=40, style='search.TEntry')
+style.configure('search.TEntry', font=('Arial', 15))
+search_bar.place(x=300, y=50)
+
+search_button = ttk.Button(weatherapp, text='Search', width=10, style='search.TButton')
+style.configure('search.TButton', font=('Arial', 8), padding=5)
+search_button.place(x=550, y=50)
+
+city_name = ttk.Label(weatherapp, text='', style='city.TLabel')
+style.configure('city.TLabel', font=('Arial', 20), foreground='white', background='blue')
+city_name.place(x=370, y=100)
+
+date_lbl = ttk.Label(weatherapp, text='', style='date.TLabel')
+style.configure('date.TLabel', font=('Arial', 10), foreground='white', background='blue')
+date_lbl.place(x=400, y=140)
+
+weather_icon = ttk.Label(weatherapp, style='icon.TLabel')
+style.configure('icon.TLabel', background='blue')
+weather_icon.place(x=400, y=200)
+
+temp_lbl = ttk.Label(weatherapp, text='', style='temp.TLabel')
+style.configure('temp.TLabel', font=('Arial', 35, 'bold'), foreground='white', background='blue')
+temp_lbl.place(x=400, y=250)
+
+desc_lbl = ttk.Label(weatherapp, text='', style='desc.TLabel')
+style.configure('desc.TLabel', font=('Arial', 15, 'bold'), foreground='white', background='blue')
+desc_lbl.place(x=400, y=310)
+
+maxmintemp_lbl = ttk.Label(weatherapp, text='', style='maxmin.TLabel')
+style.configure('maxmin.TLabel', font=('Arial', 13), foreground='white', background='blue')
+maxmintemp_lbl.place(x=400, y=350)
 
 weatherapp.mainloop()
